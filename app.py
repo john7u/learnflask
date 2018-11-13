62c499628478493475ad1e5b6e2e76ee1b6db8ca
johnauerbach <since1919@126.com>
2018-11-13 23:43:52 +0800

    + flash

--- app.py
+++ app.py
@@ -1,33 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask, make_response, render_template
+from flask import Flask, make_response, render_template, session, url_for, redirect, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
 app = Flask(__name__)
+app.config['CSRF_ENABLED'] = True
 app.config['SECRET_KEY'] = 'stephencurry30'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
 class NameForm(FlaskForm):
-    name = StringField('你的名字？', validators=[Required])
+    name = StringField('你的名字？', validators=[DataRequired()])
+    name_v = StringField('请再次输入', validators=[DataRequired()])
     submit = SubmitField('提交')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    response = make_response(render_template('index.html', current_time=datetime.utcnow(), form=form, name=name))
-    return response
+        if form.name.data == form.name_v.data:
+            flash(u'欢迎回来,{}'.format(form.name.data), 'success')
+            session['name'] = form.name.data
+        else:
+            flash(u'输入的名字不一致', 'danger')
+            session['name'] = '陌生人'
+        return redirect(url_for('index'))
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
